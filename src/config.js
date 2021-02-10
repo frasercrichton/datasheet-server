5b95296a7ad83055bfac99ae4c7601d14fa353a7
Fraser Crichton <fraser.crichton.developer@gmail.com>
2021-02-10 16:33:56 +1300

    env vars

--- src/config.js
+++ src/config.js
@@ -1,43 +1,18 @@
-// import { timemap } from './lib'
-
-// export default {
-//   gsheets: [
-//     {
-//       name: 'curfew-bell',
-//       id: '1vzF8mzb9D6anbROwp4RnB1jZj3AlRxsX-gS0gugr19o',
-//       tabs: timemap.default
-//     }
-//   ],
-// }
-
 import { timemap } from './lib'
-
+const rakeCommunitySheet = '1rH3lsBQfSTKZqFGANM4BLK0PA-xD-ykTiR2f9SPqADI'
 export default {
   gsheets: [
     {
       name: 'curfew',
-      id: '1rH3lsBQfSTKZqFGANM4BLK0PA-xD-ykTiR2f9SPqADI',
+      id: rakeCommunitySheet,
       tabs: timemap.default
     }
   ],
 }
 
-// washington: 
+// Washington DC arrest data: 
 // id: '1vzF8mzb9D6anbROwp4RnB1jZj3AlRxsX-gS0gugr19o',
 
-//ACLED
+//ACLED :
 // id: '15dQTFRkcePCHF4Nfr_JhQWz1LfYtL8_pXhom2F4opWM',
-     
-
-// import { timemap } from './lib'
-
-// export default {
-//   gsheets: [],
-//   xlsx: [
-//     {
-//       name: 'timemap_data',
-//       path: 'data/timemap_data.xlsx',
-//       tabs: timemap.default
-//     }
-//   ]
-// }
+  
