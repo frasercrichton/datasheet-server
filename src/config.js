8c6047ba39932f75703841bd2721a05811fa0526
Fraser Crichton <fraser.crichton.developer@gmail.com>
2021-02-10 13:00:05 +1300

    Update sheet

--- src/config.js
+++ src/config.js
@@ -1,14 +1,27 @@
+// import { timemap } from './lib'
+
+// export default {
+//   gsheets: [
+//     {
+//       name: 'curfew-bell',
+//       id: '1vzF8mzb9D6anbROwp4RnB1jZj3AlRxsX-gS0gugr19o',
+//       tabs: timemap.default
+//     }
+//   ],
+// }
+
 import { timemap } from './lib'
 
 export default {
   gsheets: [
     {
-      name: 'timemap_data',
-      id: '1vzF8mzb9D6anbROwp4RnB1jZj3AlRxsX-gS0gugr19o',
+      name: 'curfew',
+      id: '1rH3lsBQfSTKZqFGANM4BLK0PA-xD-ykTiR2f9SPqADI',
       tabs: timemap.default
     }
   ],
 }
+
 // washington: 
 // id: '1vzF8mzb9D6anbROwp4RnB1jZj3AlRxsX-gS0gugr19o',
 
